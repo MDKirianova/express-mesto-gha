ffa8c5dc77dfea89757575651983e4265c4d3300
Marina Kiryanova <martinkazz@yandex.ru>
2023-12-05 20:47:28 +0300

    PW-13 fix autotest bugs

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb').then(() => {
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   req.user = {
